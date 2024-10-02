38be8b4326646b30d211f7135d7f05501cc55745
Łukasz Furmańczyk <furman2004@wp.pl>
2024-10-02 20:33:52 +0200

    add new computer moves

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '. Jeśli twój ruch to kamień wygrywam! ');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce'; 
+}
+printMessage('Mój ruch: ' + computerMove);
